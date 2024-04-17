74a9aa7eff589544e8386d9d4a02e9caa0dd89fe
Juan Jose Alvarez <jjchelo665@gmail.com>
2024-04-16 19:44:09 -0500

    aritmeticos

---
+++ aritmeticos.js
@@ -0,0 +1,23 @@
+// Suma
+let resultadoSuma = 10 + 5;
+console.log("El resultado es:",resultadoSuma); // Output: 15
+// Resta
+let resultadoResta = 20 - 7;
+console.log("El resultado es:",resultadoResta); // Output: 13
+// Multiplicación
+let resultadoMultiplicacion = 3 * 4;
+console.log("El resultado es:",resultadoMultiplicacion); // Output: 12
+// División
+let resultadoDivision = 16 / 4;
+console.log("El resultado es:",resultadoDivision); // Output: 4
+// Residuo
+let resultadoResiduo = 10 % 3;
+console.log("El resultado es:",resultadoResiduo); // Output: 1
+// Incremento
+let numero = 1;
+numero++;
+console.log("El resultado es:",numero); // Output: 2
+// Decremento
+let otroNumero = 5;
+otroNumero--;
+console.log("El resultado es:",otroNumero); // Output: 4
